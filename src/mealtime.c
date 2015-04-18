2789aa0319aa6a6cbb89e3e7e9b4d01cb7044771
Turner Vink <turner.vink@me.com>
2015-04-18 11:11:20 -0700

    version 1.0 w/ color support

--- src/mealtime.c
+++ src/mealtime.c
@@ -1,13 +1,9 @@
 #include <pebble.h>
 
-#define KEY_TEMPERATURE 0
-#define KEY_CONDITIONS 1
-
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_ampm_layer;
 static TextLayer *s_date_layer;
-static TextLayer *s_weather_layer;
 static GFont s_time_font;
 static GFont s_date_font;
 
@@ -145,18 +141,10 @@ static void update_time() {
 #endif
   }
   
-  //static char time_buffer[] = "00";
   static char date_buffer[] = "WWWWWWWWW";
-    
-  /*if(clock_is_24h_style() == true) {
-    strftime(time_buffer, sizeof("00"), "%H", tick_time);
-  } else {
-    strftime(time_buffer, sizeof("00"), "%l", tick_time);
-  }*/
 
   strftime(date_buffer, sizeof("WWWWWWWWW"), "%A", tick_time);
-    	
-  //text_layer_set_text(s_time_layer, time_buffer);
+
   text_layer_set_text(s_date_layer, date_buffer); 
   
 }
@@ -166,29 +154,19 @@ static void main_window_load(Window *window) {
 	s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CYNTHE_28));
 	s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CYNTHE_18));
 	
-	s_weather_layer = text_layer_create(GRect(0, 0, 140, 168));
-	text_layer_set_background_color(s_weather_layer, GColorClear);
-	text_layer_set_text_color(s_weather_layer, GColorBlack);
-	text_layer_set_font(s_weather_layer, s_date_font);
-	text_layer_set_text(s_weather_layer, "Loading...");
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
-	
 	s_time_layer = text_layer_create(GRect(0, 128, 140, 168));
-	//text_layer_set_text_color(s_time_layer, GColorBlack);
 	text_layer_set_background_color(s_time_layer, GColorClear);
 	text_layer_set_font(s_time_layer, s_time_font);
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 	
 	s_ampm_layer = text_layer_create(GRect(0, 92, 140, 168));
-	text_layer_set_text_color(s_ampm_layer, GColorBlack);
 	text_layer_set_background_color(s_ampm_layer, GColorClear);
 	text_layer_set_font(s_ampm_layer, s_time_font);
 	text_layer_set_text_alignment(s_ampm_layer, GTextAlignmentRight);
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_ampm_layer));
 	
 	s_date_layer = text_layer_create(GRect(0, 119, 138, 168));
-	text_layer_set_text_color(s_date_layer, GColorBlack);
 	text_layer_set_background_color(s_date_layer, GColorClear);
 	text_layer_set_font(s_date_layer, s_date_font);
 	text_layer_set_text_alignment(s_date_layer, GTextAlignmentRight);
@@ -201,7 +179,6 @@ static void main_window_unload(Window *window) {
 	text_layer_destroy(s_time_layer);
 	text_layer_destroy(s_ampm_layer);
 	text_layer_destroy(s_date_layer);
-	text_layer_destroy(s_weather_layer);
 	fonts_unload_custom_font(s_time_font);
 	fonts_unload_custom_font(s_date_font);
 }
@@ -209,26 +186,6 @@ static void main_window_unload(Window *window) {
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
-
-// weather stuff
-
-static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-
-}
-
-static void inbox_dropped_callback(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
-}
-
-static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
-}
-
-static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
-}
-
-// end weather stuff
 
 static void init() {
   s_main_window = window_create();
@@ -240,13 +197,6 @@ static void init() {
 
   window_stack_push(s_main_window, true);
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  
-  app_message_register_inbox_received(inbox_received_callback);
-  app_message_register_inbox_dropped(inbox_dropped_callback);
-  app_message_register_outbox_failed(outbox_failed_callback);
-  app_message_register_outbox_sent(outbox_sent_callback);
-  
-  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
 static void deinit() {
